d44bf0ccbaba500b8b8f93af2dd739be5fab4ad7
Edi Modrić <edi@netgen.hr>
2015-09-07 15:14:45 +0200

    PHPDoc

--- search/plugins/ezplatformsearch/ezplatformsearch.php
+++ search/plugins/ezplatformsearch/ezplatformsearch.php
@@ -21,6 +21,9 @@ class eZPlatformSearch implements ezpSearchEngine
      */
     protected $searchEngine;
 
+    /**
+     * Constructor
+     */
     public function __construct()
     {
         $serviceContainer = ezpKernel::instance()->getServiceContainer();
@@ -33,7 +36,6 @@ class eZPlatformSearch implements ezpSearchEngine
     /**
      * Whether a commit operation is required after adding/removing objects.
      *
-     * @see commit()
      * @return bool
      */
     public function needCommit()
@@ -44,7 +46,6 @@ class eZPlatformSearch implements ezpSearchEngine
     /**
      * Whether calling removeObject() is required when updating an object.
      *
-     * @see removeObject()
      * @return bool
      */
     public function needRemoveWithUpdate()
@@ -55,10 +56,10 @@ class eZPlatformSearch implements ezpSearchEngine
     /**
      * Adds object $contentObject to the search database.
      *
-     * @param eZContentObject $contentObject Object to add to search engine
+     * @param \eZContentObject $contentObject Object to add to search engine
      * @param bool $commit Whether to commit after adding the object
      *
-     * @return bool True if the operation succeed.
+     * @return bool True if the operation succeeded.
      */
     public function addObject( $contentObject, $commit = true )
     {
@@ -95,10 +96,10 @@ class eZPlatformSearch implements ezpSearchEngine
      *
      * @deprecated Since 5.0, use removeObjectById()
      *
-     * @param eZContentObject $contentObject the content object to remove
+     * @param \eZContentObject $contentObject the content object to remove
      * @param bool $commit Whether to commit after removing the object
      *
-     * @return bool True if the operation succeed.
+     * @return bool True if the operation succeeded
      */
     public function removeObject( $contentObject, $commit = null )
     {
@@ -106,14 +107,14 @@ class eZPlatformSearch implements ezpSearchEngine
     }
 
     /**
-     * Removes a content object by Id from the search database.
+     * Removes a content object by ID from the search database.
      *
      * @since 5.0
      *
-     * @param int $contentObjectId The content object to remove by id
+     * @param int $contentObjectId The content object to remove by ID
      * @param bool $commit Whether to commit after removing the object
      *
-     * @return bool True if the operation succeed.
+     * @return bool True if the operation succeeded
      */
     public function removeObjectById( $contentObjectId, $commit = null )
     {
@@ -138,8 +139,6 @@ class eZPlatformSearch implements ezpSearchEngine
 
     /**
      * Searches $searchText in the search database.
-     *
-     * @see supportedSearchTypes()
      *
      * @param string $searchText Search term
      * @param array $params Search parameters
